dd37c0188667b80dbf8c1cf56eb32f6fc9855a7c
Bilal996999 <bilalidrees.bi700@gmail.com>
2024-08-10 14:14:58 +0500

    change accordion location

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 import HomeHero from "@/components/HomeHero";
 import Image from "next/image";
 import HomePage from "@/pages/HomePage"
-import 'react-bootstrap-accordion/dist/index.css';
+
 
 
 export default function Home() {
--- components/FaqAccordion.jsx
+++ components/FaqAccordion.jsx
@@ -1,71 +1,10 @@
 "use client"
 import { Accordion } from 'react-bootstrap-accordion'
+import 'react-bootstrap-accordion/dist/index.css';
 
 
 const FaqAccordion = () => {
     return (
-        // <Accordion defaultActiveKey="0" className='faq-accordion'>
-        //     <Accordion.Item eventKey="0">
-        //         <Accordion.Header>What is an Auto Audit Vehicle history report? </Accordion.Header>
-        //         <Accordion.Body className='text-[18px] text-[#666] leading-[1.5em]'>
-        //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        //             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        //             minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        //             aliquip ex ea commodo consequat. Duis aute irure dolor in
-        //             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        //             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        //             culpa qui officia deserunt mollit anim id est laborum.
-        //         </Accordion.Body>
-        //     </Accordion.Item>
-        //     <Accordion.Item eventKey="1">
-        //         <Accordion.Header>For whom do we provide Vehicle history report?</Accordion.Header>
-        //         <Accordion.Body className='text-[18px] text-[#666] leading-[1.5em]'>
-        //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        //             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        //             minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        //             aliquip ex ea commodo consequat. Duis aute irure dolor in
-        //             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        //             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        //             culpa qui officia deserunt mollit anim id est laborum.
-        //         </Accordion.Body>
-        //     </Accordion.Item>
-        //     <Accordion.Item eventKey="2">
-        //         <Accordion.Header>Do Auto Audit reports have information on every vehicle?</Accordion.Header>
-        //         <Accordion.Body className='text-[18px] text-[#666] leading-[1.5em]'>
-        //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        //             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        //             minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        //             aliquip ex ea commodo consequat. Duis aute irure dolor in
-        //             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        //             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        //             culpa qui officia deserunt mollit anim id est laborum.
-        //         </Accordion.Body>
-        //     </Accordion.Item>
-        //     <Accordion.Item eventKey="3">
-        //         <Accordion.Header>What is a VIN?</Accordion.Header>
-        //         <Accordion.Body className='text-[18px] text-[#666] leading-[1.5em]'>
-        //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        //             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        //             minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        //             aliquip ex ea commodo consequat. Duis aute irure dolor in
-        //             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        //             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        //             culpa qui officia deserunt mollit anim id est laborum.
-        //         </Accordion.Body>
-        //     </Accordion.Item>
-        //     <Accordion.Item eventKey="4">
-        //         <Accordion.Header>Where can I find my VIN number?</Accordion.Header>
-        //         <Accordion.Body className='text-[18px] text-[#666] leading-[1.5em]'>
-        //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        //             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        //             minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        //             aliquip ex ea commodo consequat. Duis aute irure dolor in
-        //             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        //             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        //             culpa qui officia deserunt mollit anim id est laborum.
-        //         </Accordion.Body>
-        //     </Accordion.Item>
-        // </Accordion>
         <div className='faq-accordion'>
             <Accordion title="What is an Auto Audit Vehicle history report?" >
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
